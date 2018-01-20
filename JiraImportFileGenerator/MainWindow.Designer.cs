8f69061e47e09b5fba9a755550f2730a5cb11bf8
brzozowski-tomasz <Tomasz.Brzozowski@redknee.com>
2018-01-20 22:20:14 +0100

    First version

---
+++ JiraImportFileGenerator/MainWindow.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace CodeCleanUpResharperToJiraConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadIssues = new System.Windows.Forms.Button();
+            this.issuesGridView = new System.Windows.Forms.DataGridView();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnGenerateJira = new System.Windows.Forms.Button();
+            this.lblNumberOfIssues = new System.Windows.Forms.Label();
+            this.lblNumberOfIssuesSelected = new System.Windows.Forms.Label();
+            this.cmbxIssueType = new System.Windows.Forms.ComboBox();
+            this.cmbxIssueCategory = new System.Windows.Forms.ComboBox();
+            this.btnLoadCodeAnalysis = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.issuesGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadIssues
+            // 
+            this.btnLoadIssues.Location = new System.Drawing.Point(29, 27);
+            this.btnLoadIssues.Name = "btnLoadIssues";
+            this.btnLoadIssues.Size = new System.Drawing.Size(165, 30);
+            this.btnLoadIssues.TabIndex = 0;
+            this.btnLoadIssues.Text = "Load R# Issues";
+            this.btnLoadIssues.UseVisualStyleBackColor = true;
+            this.btnLoadIssues.Click += new System.EventHandler(this.btnLoadIssues_Click);
+            // 
+            // issuesGridView
+            // 
+            this.issuesGridView.AllowUserToAddRows = false;
+            this.issuesGridView.AllowUserToDeleteRows = false;
+            this.issuesGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.issuesGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.issuesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.issuesGridView.Location = new System.Drawing.Point(29, 108);
+            this.issuesGridView.Name = "issuesGridView";
+            this.issuesGridView.ReadOnly = true;
+            this.issuesGridView.RowTemplate.Height = 24;
+            this.issuesGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.issuesGridView.Size = new System.Drawing.Size(1180, 525);
+            this.issuesGridView.TabIndex = 1;
+            this.issuesGridView.SelectionChanged += new System.EventHandler(this.issuesGridView_SelectionChanged);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFilter.Location = new System.Drawing.Point(1134, 57);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 3;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnGenerateJira
+            // 
+            this.btnGenerateJira.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateJira.Location = new System.Drawing.Point(1043, 657);
+            this.btnGenerateJira.Name = "btnGenerateJira";
+            this.btnGenerateJira.Size = new System.Drawing.Size(166, 28);
+            this.btnGenerateJira.TabIndex = 4;
+            this.btnGenerateJira.Text = "Generate Jira Csv";
+            this.btnGenerateJira.UseVisualStyleBackColor = true;
+            this.btnGenerateJira.Click += new System.EventHandler(this.btnGenerateJira_Click);
+            // 
+            // lblNumberOfIssues
+            // 
+            this.lblNumberOfIssues.AutoSize = true;
+            this.lblNumberOfIssues.Location = new System.Drawing.Point(26, 76);
+            this.lblNumberOfIssues.Name = "lblNumberOfIssues";
+            this.lblNumberOfIssues.Size = new System.Drawing.Size(122, 17);
+            this.lblNumberOfIssues.TabIndex = 5;
+            this.lblNumberOfIssues.Text = "Number of Issues:";
+            // 
+            // lblNumberOfIssuesSelected
+            // 
+            this.lblNumberOfIssuesSelected.AutoSize = true;
+            this.lblNumberOfIssuesSelected.Location = new System.Drawing.Point(279, 76);
+            this.lblNumberOfIssuesSelected.Name = "lblNumberOfIssuesSelected";
+            this.lblNumberOfIssuesSelected.Size = new System.Drawing.Size(179, 17);
+            this.lblNumberOfIssuesSelected.TabIndex = 6;
+            this.lblNumberOfIssuesSelected.Text = "Number of Issues selected:";
+            // 
+            // cmbxIssueType
+            // 
+            this.cmbxIssueType.FormattingEnabled = true;
+            this.cmbxIssueType.Location = new System.Drawing.Point(642, 57);
+            this.cmbxIssueType.Name = "cmbxIssueType";
+            this.cmbxIssueType.Size = new System.Drawing.Size(479, 24);
+            this.cmbxIssueType.TabIndex = 7;
+            // 
+            // cmbxIssueCategory
+            // 
+            this.cmbxIssueCategory.FormattingEnabled = true;
+            this.cmbxIssueCategory.Location = new System.Drawing.Point(642, 27);
+            this.cmbxIssueCategory.Name = "cmbxIssueCategory";
+            this.cmbxIssueCategory.Size = new System.Drawing.Size(479, 24);
+            this.cmbxIssueCategory.TabIndex = 8;
+            this.cmbxIssueCategory.SelectedIndexChanged += new System.EventHandler(this.cmbxIssueCategory_SelectedIndexChanged);
+            // 
+            // btnLoadCodeAnalysis
+            // 
+            this.btnLoadCodeAnalysis.Location = new System.Drawing.Point(221, 27);
+            this.btnLoadCodeAnalysis.Name = "btnLoadCodeAnalysis";
+            this.btnLoadCodeAnalysis.Size = new System.Drawing.Size(165, 30);
+            this.btnLoadCodeAnalysis.TabIndex = 9;
+            this.btnLoadCodeAnalysis.Text = "Load VS Analysis";
+            this.btnLoadCodeAnalysis.UseVisualStyleBackColor = true;
+            this.btnLoadCodeAnalysis.Click += new System.EventHandler(this.btnLoadCodeAnalysis_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(543, 657);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(479, 24);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 707);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnLoadCodeAnalysis);
+            this.Controls.Add(this.cmbxIssueCategory);
+            this.Controls.Add(this.cmbxIssueType);
+            this.Controls.Add(this.lblNumberOfIssuesSelected);
+            this.Controls.Add(this.lblNumberOfIssues);
+            this.Controls.Add(this.btnGenerateJira);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.issuesGridView);
+            this.Controls.Add(this.btnLoadIssues);
+            this.Name = "Form1";
+            this.Text = "Resharper to Jira Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.issuesGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadIssues;
+        private System.Windows.Forms.DataGridView issuesGridView;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnGenerateJira;
+        private System.Windows.Forms.Label lblNumberOfIssues;
+        private System.Windows.Forms.Label lblNumberOfIssuesSelected;
+        private System.Windows.Forms.ComboBox cmbxIssueType;
+        private System.Windows.Forms.ComboBox cmbxIssueCategory;
+        private System.Windows.Forms.Button btnLoadCodeAnalysis;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ JiraImportFileGenerator/MainWindow.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodeCleanUpResharperToJiraConverter
+{
+    public partial class Form1 : Form
+    {
+        #region Consts
+
+        private const string FileDialogXmlFilter = "Xml files | *.xml";
+        private const string IssueTable = "Issue";
+        private const string IssueTypeTable = "IssueType";
+        private const string CategoryColumn = "Category";
+
+        private const string JiraCsvExtension = ".Jira.csv";
+
+        private const string IdColumn = "Id";
+
+        private const string VStudioCodeAnalysis = "VStudioCodeAnalysis";
+
+        private const string Resharper = "Resharper";
+
+        private const string ActivationPortalUrl = "https://github.com/DFRedKnee/turnkey-converged-billing-activation-portal.git";
+        private const string WebSelfCareUrl = "https://github.com/DFRedKnee/turnkey-converged-billing-web-self-care.git";
+
+
+        #endregion
+        #region Private fields
+
+        private string _filePath;
+        private DataSet _dataSet;
+
+        private string _inputType = "";
+
+        #endregion
+        #region Consts
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+        #region Event handlers
+
+        private void btnLoadIssues_Click(object sender, EventArgs e)
+        {
+            _inputType = Resharper;
+
+            var dialog = new OpenFileDialog
+            {
+                Filter = FileDialogXmlFilter,
+                Multiselect = false
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                _filePath = dialog.FileName;
+
+                _dataSet = new DataSet();
+                _dataSet.ReadXml(_filePath);
+
+                PopulateIssuesGrid();
+                PopulateCategoryCombo();
+
+                UpdateLabels();
+            }
+        }
+
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(cmbxIssueType.Text))
+            {
+                var table = issuesGridView.DataSource as DataTable;
+                if (table != null)
+                {
+                    table.DefaultView.RowFilter = string.Empty;
+                }
+            }
+            else
+            {
+                var whereClause = $"TypeId='{cmbxIssueType.Text}'";
+                var table = issuesGridView.DataSource as DataTable;
+                if (table != null)
+                {
+                    table.DefaultView.RowFilter = whereClause;
+                }
+            }
+
+            UpdateLabels();
+        }
+
+        private void btnGenerateJira_Click(object sender, EventArgs e)
+        {
+            var outputStringBuilder = new StringBuilder();
+
+            outputStringBuilder.Append(GetHeaderRow());
+
+            foreach (DataGridViewRow issueRow in issuesGridView.SelectedRows)
+            {
+                outputStringBuilder.Append(GetIssueStringRowRepresentation(issueRow));
+            }
+
+            var outputFilePath = _filePath + JiraCsvExtension;
+            File.WriteAllText(outputFilePath, outputStringBuilder.ToString());
+
+            MessageBox.Show($"Jira import file created successfully: {outputFilePath}", "File created", MessageBoxButtons.OK);
+        }
+
+        private void cmbxIssueCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(cmbxIssueCategory.Text))
+            {
+                var table = issuesGridView.DataSource as DataTable;
+                if (table != null)
+                {
+                    table.DefaultView.RowFilter = string.Empty;
+                }
+            }
+            else
+            {
+                var whereClause = $"Category='{cmbxIssueCategory.Text}'";
+
+                cmbxIssueType.DataSource = _dataSet.Tables[IssueTypeTable].Select(whereClause).Select(item => item[IdColumn]).Distinct().ToList();
+            }
+        }
+
+        #endregion
+        #region Private methods
+
+        private string GetIssueStringRowRepresentation(DataGridViewRow issueRow)
+        {
+            var issueStringBuilder = new StringBuilder();
+
+            issueStringBuilder.Append("Code Cleanup, Redknee, Multiproduct, " + WebSelfCareUrl + ", master, ");
+
+            if (_inputType == Resharper)
+            {
+                var jiraIssueType = MapResharperTypeIdToJiraIssueType(issueRow.Cells["TypeId"].Value.ToString());
+                issueStringBuilder.Append(jiraIssueType + ',');
+                issueStringBuilder.Append(issueRow.Cells["Message"].Value + " in " + issueRow.Cells["File"].Value + ',');
+                issueStringBuilder.Append(issueRow.Cells["Message"].Value + " in " + issueRow.Cells["File"].Value + " Line: " +
+                                          issueRow.Cells["Line"].Value + ',');
+                issueStringBuilder.Append("\r\n");
+            }
+
+            else if (_inputType == VStudioCodeAnalysis)
+            {
+
+                //var jiraIssueType = MapResharperTypeIdToJiraIssueType(issueRow.Cells["TypeId"].Value.ToString());
+                issueStringBuilder.Append("Long Methods" + ',');
+                issueStringBuilder.Append("Long Methods in " + issueRow.Cells["Namespace"].Value + '.' + issueRow.Cells["Type"].Value + ".cs" + ',');
+                issueStringBuilder.Append("Long Methods in " + issueRow.Cells["Namespace"].Value + '.' + issueRow.Cells["Type"].Value + '.' + issueRow.Cells["Member"].Value.ToString().Replace(',',' ') + 
+                    " Visual studio line count: " + issueRow.Cells["LOC"].Value);
+                issueStringBuilder.Append("\r\n");
+            }
+
+            return issueStringBuilder.ToString();
+        }
+
+        private string GetHeaderRow()
+        {
+            return "Project, CDC-Company, CDC-Software Product Name, CDC-scm_url, CSC-scm_branch, Type, Title, Description\r\n";
+        }
+
+        private void UpdateLabels()
+        {
+            lblNumberOfIssues.Text = $"Number of Issues: {issuesGridView.Rows.Count}";
+            lblNumberOfIssuesSelected.Text = $"Number of Issues selected: {issuesGridView.SelectedRows.Count}";
+        }
+
+        private string MapResharperTypeIdToJiraIssueType(string resharperIssueType)
+        {
+            switch (resharperIssueType)
+            {
+                case "PossibleNullReferenceException":
+                    return "Symbolic Execution - NPE";
+                default:
+                    return "Not known Jira Type";
+            }
+        }
+
+        private void PopulateCategoryCombo()
+        {
+            cmbxIssueCategory.DataSource = _dataSet.Tables[IssueTypeTable].Select().Select(item => item[CategoryColumn]).Distinct().ToList();
+        }
+
+        private void PopulateIssuesGrid()
+        {
+            if (_inputType == VStudioCodeAnalysis)
+            {
+                issuesGridView.DataSource = _dataSet.Tables[0];
+                return;
+            }
+
+            if (_inputType == Resharper)
+            {
+                issuesGridView.DataSource = _dataSet.Tables[IssueTable];
+            }
+        }
+
+        private void issuesGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            UpdateLabels();
+        }
+
+        #endregion Private methods
+
+        private void btnLoadCodeAnalysis_Click(object sender, EventArgs e)
+        {
+            _inputType = VStudioCodeAnalysis;
+
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Csv files | *.csv",
+                Multiselect = false
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                _filePath = dialog.FileName;
+
+                _dataSet = new DataSet();
+                var dataTable = GetDataTableFromCsv(_filePath, false);
+                _dataSet.Tables.Add(dataTable);
+                //_dataSet.ReadXml(_filePath);
+
+                PopulateIssuesGrid();
+                //PopulateCategoryCombo();
+
+                UpdateLabels();
+            }
+        }
+
+        static DataTable GetDataTableFromCsv(string path, bool isFirstRowHeader)
+        {
+            string header = isFirstRowHeader ? "Yes" : "No";
+
+            string pathOnly = Path.GetDirectoryName(path);
+            string fileName = Path.GetFileName(path);
+
+            string sql = @"SELECT * FROM [" + fileName + "]";
+
+            using (OleDbConnection connection = new OleDbConnection(
+                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathOnly +
+                ";Extended Properties=\"Text;HDR=" + header + "\""))
+            using (OleDbCommand command = new OleDbCommand(sql, connection))
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+            {
+                DataTable dataTable = new DataTable();
+                dataTable.Locale = CultureInfo.CurrentCulture;
+                adapter.Fill(dataTable);
+
+                dataTable.Columns[0].ColumnName = "Scope";
+                dataTable.Columns[1].ColumnName = "Project";
+                dataTable.Columns[2].ColumnName = "Namespace";
+                dataTable.Columns[3].ColumnName = "Type";
+                dataTable.Columns[4].ColumnName = "Member";
+                dataTable.Columns[5].ColumnName = "Maintain index";
+                dataTable.Columns[6].ColumnName = "Complexity";
+                dataTable.Columns[7].ColumnName = "Inheritance Depth";
+                dataTable.Columns[8].ColumnName = "Class coupling";
+                dataTable.Columns[9].ColumnName = "LOC";
+
+                return dataTable;
+            }
+        }
+    }
+}
